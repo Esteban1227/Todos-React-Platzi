9b5fb7ba994ddccbc2bc9e429adfd5cb531840f6
Esteban Charria Muñoz <charriaesteban9@gmail.com>
2022-07-15 16:00:13 -0500

    Updates

---
+++ precache-manifest.22919c922067737daa42fc68b51e2534.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "043297d4d6bb808a497a",
+    "url": "/Todos-React-Platzi/static/css/main.ac4e650e.chunk.css"
+  },
+  {
+    "revision": "043297d4d6bb808a497a",
+    "url": "/Todos-React-Platzi/static/js/main.043297d4.chunk.js"
+  },
+  {
+    "revision": "f5241a0232e0464995cb",
+    "url": "/Todos-React-Platzi/static/js/1.f5241a02.chunk.js"
+  },
+  {
+    "revision": "d82bb5b5e3fbb35ddf2c",
+    "url": "/Todos-React-Platzi/static/js/runtime~main.d82bb5b5.js"
+  },
+  {
+    "revision": "4ca744a69a53e393531adf4152bac523",
+    "url": "/Todos-React-Platzi/index.html"
+  }
+];
